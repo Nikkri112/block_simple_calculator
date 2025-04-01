53c3b8296b2b45cbd413021b59a794fcf82a7dde
Nikki Raii <nikkiraii@yandex.ru>
2025-04-01 17:45:58 +0700

    попытка фикса 1

--- block_simple_calculator.php
+++ block_simple_calculator.php
@@ -1,3 +1,4 @@
+
 <?php
 // This file is part of Moodle - https://moodle.org/
 //
@@ -38,13 +39,14 @@ class block_simple_calculator extends block_base {
      * @return stdClass The block contents.
      */
     public function get_content() {
-        //require(__DIR__ . '/config.php');
         if ($this->content !== null) {
             return $this->content;
         }
         global $DB;
         global $PAGE;
         global $USER;
+        $this->content = new stdClass();
+        $this->content->text = "";
         $context = $PAGE->context;
         $courseID = intval(get_config('simple_calculator','courseId_setting'));
         $altCourseID = intval(get_config('simple_calculator','altCourseId_setting'));
@@ -57,6 +59,8 @@ class block_simple_calculator extends block_base {
         $altdpoquizids = [];
         $uniqueString = get_config('simple_calculator','uniqueString_setting');
 
+
+        //Получаем id тестов курса преподавателей
         foreach($courseQuizes as $quiz){
             foreach($testnames as $name){
                 if(str_contains($quiz->name,$name) && str_contains($quiz->name,$uniqueString)){
@@ -65,6 +69,8 @@ class block_simple_calculator extends block_base {
             }
         }
 
+
+        //Получаем id тестов курса сотрудников
         foreach($altCourseQuizes as $quiz){
             foreach($testnames as $name){
                 if(str_contains($quiz->name,$name) && str_contains($quiz->name,$uniqueString)){
@@ -73,6 +79,8 @@ class block_simple_calculator extends block_base {
             }
         }
 
+
+         //Получаем id дпо тестов курса преподавателей
         foreach($courseQuizes as $quiz){
             foreach($testnames as $name){
                 if(str_contains($quiz->name,$name) && str_contains($quiz->name,'*')){
@@ -81,6 +89,8 @@ class block_simple_calculator extends block_base {
             }
         }
 
+
+        //Получаем id дпо тестов курса сотрудников
         foreach($altCourseQuizes as $quiz){
             foreach($testnames as $name){
                 if(str_contains($quiz->name,$name) && str_contains($quiz->name,'*')){
@@ -88,6 +98,9 @@ class block_simple_calculator extends block_base {
                 }
             }
         }
+
+
+        //Получаем попытки прохождения одного из тестов
         $quiz_attempt = $DB->get_record('quiz_attempts', array('quiz' => $quizIds[0]));
         $question_usageid = $quiz_attempt->uniqueid;
         $question_attempt = $DB->get_records('question_attempts',array('questionusageid'=>$question_usageid));
@@ -96,6 +109,9 @@ class block_simple_calculator extends block_base {
                 $question_attempt = $qa->questionid;
             }
         }
+
+        //Получаем список ответов на первый вопрос
+
         $question_answers_obj = $DB->get_records('question_answers',array('question'=>$question_attempt));
         $question_answers = [];
         foreach ($question_answers_obj as $qa){
@@ -103,13 +119,19 @@ class block_simple_calculator extends block_base {
                 array_push($question_answers,$qa->answer);
             }
         }
-        if ($context->contextlevel == CONTEXT_USER) {
-            $userid = $context->instanceid; // ID пользователя, чей профиль просматривается
-        } else {
-            // Не в контексте профиля пользователя
-            $userid = $USER->id;
+        
+        //ID пользователя в зависимости от контекста
+
+        try {
+            // Получаем ID пользователя с автоматической валидацией
+            $userid = $this->get_user_id_from_course_context();
+        } catch (moodle_exception $e) {
+            $this->content->text = $this->render_error($e);
         }
-        //Проверяем если есть попытка в педагогическом курсе
+
+
+        //Выявляем к какому курсу принадлежит пользователь
+
         $isAlt = true;
         foreach($quizIds as $testid){
             $attempts = $DB->get_records('quiz_attempts', array('quiz' => $testid));
@@ -122,6 +144,9 @@ class block_simple_calculator extends block_base {
         if($isAlt){
             $quizIds = $altQuizIds;
         }
+
+
+        //Функция для получения результатов
 
         function aquire_results($testid,$testname,$userid,$answer=NULL){
             global $DB;
@@ -197,7 +222,6 @@ class block_simple_calculator extends block_base {
                 $quizYear=substr($quiz->name,strpos($quiz->name,'*')-4,4);
             }
             //Считаем результаты теста в процентах
-
             $finalgrade = ($finalgrade/$maxGrade)*100;
             $medianfinal = ($medianfinal/$maxGrade)*100;
             $results = (object) 
@@ -227,7 +251,6 @@ class block_simple_calculator extends block_base {
                     $quizname = $name;
                 }
             }
-            
             array_push($quizResults->resultarray, aquire_results($quizid,$quizname,$userid));
             foreach($question_answers as $qa){
                 array_push($quizResults->responsearray,aquire_results($quizid,$quizname,$userid,$qa));
@@ -254,29 +277,13 @@ class block_simple_calculator extends block_base {
                 }
             }
         }
-
-        $this->content = new stdClass();
-        $this->content->text = '';
-        $badIndexes = [];
-        for($i=0;$i<count($quizResults->resultarray);$i++){
-            for($j=$i;$j<count($quizResults->resultarray);$j++){
-                if($quizResults->resultarray[$i]->quizId!=$quizResults->resultarray[$j]->quizId){
-                    if(($quizResults->resultarray[$i]->testName==$quizResults->resultarray[$j]->testName) && ($quizResults->resultarray[$i]->year==$quizResults[$j]->year)){
-                        if($quizResults->resultarray[$i]->timecreated>=$quizResults->resultarray[$j]->timecreated){
-                            array_push($badIndexes,$j);
-                        }
-                        else{
-                            array_push($badIndexes,$i);
-                        }
-                    }
-                }
-            }
-        }
-        foreach($badIndexes as $index){
-            unset($quizResults->resultarray[$index]);
-        } 
+        
+        
+        
         $quizResults->resultarray = array_values($quizResults->resultarray);
-        //НУЖНОЕ
+        
+
+        //Настройка рендерера
         $renderer = $this->page->get_renderer('block_simple_calculator');
         if(count($quizResults->resultarray)!=0){
             $this->content->text .= $renderer->render_calculator($quizResults);
@@ -284,11 +291,117 @@ class block_simple_calculator extends block_base {
         else{
             $this->content->text .= "Что то пошло не так";
         }
+        //Вывод контента
         return $this->content;
     }
     function has_config() {
         return true;
     }
+
+    private function validate_context_and_user($context, $default_userid) {
+        // 1. Validate context object structure
+        if (!is_object($context)) {
+            throw new moodle_exception('invalidcontextobject', 'block_simple_calculator');
+        }
+        
+        // 3. Validate context level
+        $valid_context_levels = [CONTEXT_SYSTEM, CONTEXT_COURSE, CONTEXT_MODULE, CONTEXT_BLOCK, CONTEXT_USER];
+        if (!in_array($context->contextlevel, $valid_context_levels)) {
+            throw new moodle_exception('invalidcontextlevel', 'block_simple_calculator');
+        }
+        
+        // 4. Determine user ID based on context
+        $userid = $default_userid;
+        if ($context->contextlevel == CONTEXT_USER) {
+            $userid = $context->instanceid;
+            
+            // Additional validation for user context
+            if ($userid == $default_userid) {
+                throw new moodle_exception('selfcontextonly', 'block_simple_calculator');
+            }
+        }
+        
+        // 5. Validate user ID
+        return $this->validate_user_id($userid);
+    }
+    
+    /**
+     * Validate user ID
+     * 
+     * @param int $userid User ID to validate
+     * @return int Validated user ID
+     * @throws moodle_exception
+     */
+    private function validate_user_id($userid) {
+        global $DB;
+        
+        // 1. Basic type and range check
+        if (!is_number($userid) || $userid <= 0) {
+            throw new moodle_exception('invaliduserid', 'block_simple_calculator');
+        }
+        
+        // 2. Check if user exists in database
+        if (!$DB->record_exists('user', ['id' => $userid, 'deleted' => 0])) {
+            throw new moodle_exception('usernotfound', 'block_simple_calculator');
+        }
+        
+        // 3. Check if user is suspended
+        if ($DB->get_field('user', 'suspended', ['id' => $userid])) {
+            throw new moodle_exception('usersuspended', 'block_simple_calculator');
+        }
+        
+        return (int)$userid;
+    }
+
+    private function render_error($exception) {
+        return html_writer::div(
+            get_string('erroroccurred', 'block_simple_calculator') . $exception->getMessage(),
+            'alert alert-danger'
+        );
+    }
+
+    protected function get_user_id_from_course_context() {
+        global $PAGE, $USER;
+        
+        $context = $PAGE->context;
+        
+        // 2. Check context level and get user ID
+        switch ($context->contextlevel) {
+            case CONTEXT_COURSE:
+                // For course context - always return current user
+                return $this->validate_user_id($USER->id);
+                
+            case CONTEXT_USER:
+                // For user profile context - return profile owner
+                return $this->validate_user_id($context->instanceid);
+                
+            case CONTEXT_BLOCK:
+                // For block context - check parent contexts
+                return $this->get_user_id_from_block_context($context);
+                
+            default:
+                throw new moodle_exception('unsupportedcontext', 'block_simple_calculator', '', context_helper::get_level_name($context->contextlevel));
+        }
+    }
+    
+    private function get_user_id_from_block_context($context) {
+        global $USER;
+        
+        // Get parent context (course or user)
+        $parentcontext = $context->get_parent_context();
+        
+        switch ($parentcontext->contextlevel) {
+            case CONTEXT_COURSE:
+                return $this->validate_user_id($USER->id);
+                
+            case CONTEXT_USER:
+                return $this->validate_user_id($parentcontext->instanceid);
+                
+            default:
+                throw new moodle_exception('invalidparentcontext', 'block_simple_calculator');
+        }
+    }
+    
 
     /**
      * Defines configuration data.
